6cd1f73772e7348932cace539881a6aa2cbccf57
TedieHanif <tedie.muhammad@student.umn.ac.id>
2021-03-10 13:15:14 +0700

    assignment

---
+++ Week04_33597/app/src/main/java/umn/ac/id/week4_33597/SecondActivity.java
@@ -0,0 +1,15 @@
+package umn.ac.id.week4_33597;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
